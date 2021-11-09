e4ce395afe88e92caa0e6ecdf819ad4d0b3b1af4
Jaime <jamoncayop@unal.edu.co>
2021-11-09 06:54:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const[url,setUrl]=useState([]);
+  const[isError,setIsError]=useState(false)//new
+  const[data,setData]=useState([]);
+  console.log(url);
+
+
+
+  useEffect(()=>{
+    const url =fetch("https://jsonplaceholder.typicode.com/users");
+    url
+    .then((respuesta)=> {
+      return respuesta.json();
+    })
+    .then((respuesta) =>{
+      setUrl(respuesta);
+    }) 
+    .catch((error) =>{
+      console.log(error);
+    });
+  },[]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola Mundo
+      {url.map((url)=>{
+        return(
+          <div key={url.id}>
+            <span>{url.name}</span>
+          </div>
+        )
+      })}
+      {/* {isError? <h3>Error!</h3>: null}  nuevo */}
     </div>
   );
 }
